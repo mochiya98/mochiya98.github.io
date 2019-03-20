fc4d21d5cbf9a688187336e8a29c521b55292d0d
Yukimasa Funaoka <yukimasafunaoka@gmail.com>
2019-03-20 17:33:16 +0900

    optimize: enable loose destructuring

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ["@babel/preset-env"],
-  plugins: ["@babel/plugin-syntax-dynamic-import"]
+  plugins: [
+    "@babel/plugin-syntax-dynamic-import",
+    ["@babel/plugin-transform-destructuring", { loose: true }]
+  ]
 };
