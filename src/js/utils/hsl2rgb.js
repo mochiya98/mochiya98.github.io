428c28d9c598925ec7c86ac1e231d8288b03c391
Yukimasa funaoka <yukimasafunaoka@gmail.com>
2019-06-03 08:47:15 +0900

    change types

--- src/js/utils/hsl2rgb.js
+++ src/js/utils/hsl2rgb.js
@@ -1,11 +1,11 @@
 const hue = (p, q, t) => {
   if (t < 0) t += 1;
   if (t > 1) t -= 1;
-  if (t < 1/6) return p + (q - p) * 6 * t;
-  if (t < 1/2) return q;
-  if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
   return p;
-}
+};
 const hsl2rgb = (h, s, l) => {
   let r, g, b;
   h = h / 360;
@@ -16,16 +16,12 @@ const hsl2rgb = (h, s, l) => {
     const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
     const p = 2 * l - q;
 
-    r = hue(p, q, h + 1/3);
+    r = hue(p, q, h + 1 / 3);
     g = hue(p, q, h);
-    b = hue(p, q, h - 1/3);
+    b = hue(p, q, h - 1 / 3);
   }
 
-  return (
-    (Math.round(r * 255)<<16) +
-    (Math.round(g * 255)<<8) +
-    (Math.round(b * 255))
-  )>>>0;
-}
+  return [r, g, b];
+};
 
 export default hsl2rgb;
