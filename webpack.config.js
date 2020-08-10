5c4c092a28e31f647ddee03e4f6b575e51acfe1c
Yukimasa funaoka <yukimasafunaoka@gmail.com>
2020-08-10 20:56:02 +0900

    refactor build cfg

--- webpack.config.js
+++ webpack.config.js
@@ -5,110 +5,111 @@ const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
 const WebpackBar = require("webpackbar");
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  entry: { app: "./src/js/app.ts" },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: "html-loader",
-      },
-      {
-        test: /\.css/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              url: false,
-              sourceMap: true,
+module.exports = (env, argv) => {
+  const isDevelopment =
+    argv.mode !== "production" && process.env.NODE_ENV !== "production";
+  return {
+    mode: isDevelopment ? "development" : "production",
+    entry: { app: "./src/js/app.ts" },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          use: [
+            {
+              loader: "babel-loader",
             },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              plugins: [require("autoprefixer")(), require("cssnano")()],
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-  },
-  output: {
-    path: __dirname + "/dist",
-    chunkFilename: "js/chunks/[name].js",
-    publicPath: "./",
-    filename: "js/[name].js",
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: true,
-        cache: true,
-        sourceMap: true,
-        terserOptions: {
-          sourceMap: true,
-          mangle: {
-            properties: {
-              reserved: [
-                "draw",
-                "modelViewMatrix",
-                "projectionMatrix",
-                "normal",
-                "normalMatrix",
-                //WebFont Loader
-                "google",
-                "families",
-                "text",
-                //oes-vertex-attrib-array-polyfill
-                "VERTEX_ARRAY_BINDING_OES",
-                "createVertexArrayOES",
-                "deleteVertexArrayOES",
-                "isVertexArrayOES",
-                "bindVertexArrayOES",
-                "__OESVertexArrayObject",
-              ],
-            },
-          },
+          ],
         },
-      }),
-    ],
-    splitChunks: {
-      cacheGroups: {
-        vendors: false,
+        {
+          test: /\.css/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader",
+              options: {
+                url: false,
+                sourceMap: isDevelopment,
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: isDevelopment,
+                plugins: [require("autoprefixer")(), require("cssnano")()],
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    },
+    output: {
+      path: __dirname + "/dist",
+      chunkFilename: "js/chunks/[name].js",
+      publicPath: "./",
+      filename: "js/[name].js",
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          cache: true,
+          sourceMap: isDevelopment,
+          terserOptions: {
+            sourceMap: isDevelopment,
+            mangle: {
+              properties: {
+                reserved: [
+                  "draw",
+                  "modelViewMatrix",
+                  "projectionMatrix",
+                  "normal",
+                  "normalMatrix",
+                  //WebFont Loader
+                  "google",
+                  "families",
+                  "text",
+                  //oes-vertex-attrib-array-polyfill
+                  "VERTEX_ARRAY_BINDING_OES",
+                  "createVertexArrayOES",
+                  "deleteVertexArrayOES",
+                  "isVertexArrayOES",
+                  "bindVertexArrayOES",
+                  "__OESVertexArrayObject",
+                ],
+              },
+            },
+          },
+        }),
+      ],
+      splitChunks: {
+        cacheGroups: {
+          vendors: false,
+        },
       },
     },
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    publicPath: "/",
-    inline: true,
-    open: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      defaultAttribute: "async",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css",
-    }),
-    new WebpackBar(),
-  ],
+    devtool: isDevelopment ? "inline-source-map" : false,
+    devServer: {
+      contentBase: "./dist",
+      publicPath: "/",
+      inline: true,
+      open: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new ScriptExtHtmlWebpackPlugin({
+        defaultAttribute: "async",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].css",
+        chunkFilename: "css/[id].css",
+      }),
+      new WebpackBar(),
+    ],
+  };
 };
